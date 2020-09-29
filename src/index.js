77d445a128f500e09c1fec07bd134e2f3b4a0c96
johkli <johannes.klint@etraveligroup.com>
2020-09-29 21:43:06 +0200

    Refactor to use only one context

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React, { useState, createElement, useCallback, useMemo } from 'react'
 import { createContext, useContextSelector } from 'use-context-selector'
 
-let keyCount = 0 // global key count for all fire's
+let idCount = 0
 
 const ActionsContext = createContext(null)
-const StateContext = createContext(null)
 
 export function Provider({ children }) {
   const initState = new Map()
@@ -14,7 +13,7 @@ export function Provider({ children }) {
       read: (val) => {
         const initRender = !state.get(val.id)
         if (initRender) {
-          state.set(val.id, val.init)
+          state.set(val.id, val.initState)
           return state.get(val.id)
         }
         return state.get(val.id)
@@ -27,60 +26,39 @@ export function Provider({ children }) {
   )
   return createElement(
     ActionsContext.Provider,
-    { value: actions },
-    createElement(StateContext.Provider, { value: state }, children)
+    { value: { actions, state } },
+    children
   )
 }
 
-export function fire(read, write) {
-  const config = {
-    id: ++keyCount,
-  }
-  if (typeof read === 'function') {
-    config.read = read
-  } else {
-    config.init = read
-    config.read = (get) => {
-      return get(config)
-    }
-    config.write = (get, set, update) => {
-      set(config, typeof update === 'function' ? update(get(config)) : update)
-    }
-  }
-  if (write) {
-    config.write = write
-  }
-
-  return config
+export function fire(initState) {
+  return { id: ++idCount, initState }
 }
 
 export function useFire(fire) {
-  const value = useContextSelector(
+  const actionsContext = useContextSelector(
     ActionsContext,
     useCallback(
-      (state) => {
-        const initState = state.read(fire)
-        return { initState, ...state, id: fire.id }
+      ({ actions, state }) => {
+        const prevStateOrInitState = actions.read(fire)
+        return { prevStateOrInitState, ...actions, ...state, id: fire.id }
       },
       [fire]
     )
   )
-  const stateContext = useContextSelector(StateContext, (x) => {
-    if (x.has(value.id)) {
-      return value.initState
-    }
-    return x
-  })
 
   const setFire = useCallback(
-    (update) => {
-      if (typeof update === 'function') {
-        return value.write(update(stateContext), value.id)
+    (updateState) => {
+      if (typeof updateState === 'function') {
+        return actionsContext.write(
+          updateState(actionsContext.prevStateOrInitState),
+          actionsContext.id
+        )
       }
-      return value.write(update, value.id)
+      return actionsContext.write(updateState, actionsContext.id)
     },
-    [value, stateContext]
+    [actionsContext]
   )
-
-  return [stateContext, setFire]
+  const state = actionsContext.prevStateOrInitState
+  return [state, setFire]
 }
